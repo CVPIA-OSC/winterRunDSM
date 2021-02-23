ccd681710a695cab21dae426336771be0f23329c
emanuel <erodriguez@flowwest.com>
2021-02-23 11:46:02 -0800

    updates arg list of function to be more clear

--- R/ocean_entry_success.R
+++ R/ocean_entry_success.R
@@ -16,7 +16,19 @@
 #' @export
 ocean_entry_success <- function(migrants, month, avg_ocean_transition_month,
                                 length = c(-0.0897309864, -0.0709704348, -0.0208590732, 0.0732620916),
-                                betas = c(matrix(-2.9839253, 31, dimnames = list(watershed_labels, "intercept betas")), months = 0.35)){
+                                betas = c(`Upper Sacramento River` = -2.9839253, `Antelope Creek` = -2.9839253, 
+                                          `Battle Creek` = -2.9839253, `Bear Creek` = -2.9839253, `Big Chico Creek` = -2.9839253, 
+                                          `Butte Creek` = -2.9839253, `Clear Creek` = -2.9839253, `Cottonwood Creek` = -2.9839253, 
+                                          `Cow Creek` = -2.9839253, `Deer Creek` = -2.9839253, `Elder Creek` = -2.9839253, 
+                                          `Mill Creek` = -2.9839253, `Paynes Creek` = -2.9839253, `Stony Creek` = -2.9839253, 
+                                          `Thomes Creek` = -2.9839253, `Upper-mid Sacramento River` = -2.9839253, 
+                                          `Sutter Bypass` = -2.9839253, `Bear River` = -2.9839253, `Feather River` = -2.9839253, 
+                                          `Yuba River` = -2.9839253, `Lower-mid Sacramento River` = -2.9839253, 
+                                          `Yolo Bypass` = -2.9839253, `American River` = -2.9839253, `Lower Sacramento River` = -2.9839253, 
+                                          `Calaveras River` = -2.9839253, `Cosumnes River` = -2.9839253, 
+                                          `Mokelumne River` = -2.9839253, `Merced River` = -2.9839253, 
+                                          `Stanislaus River` = -2.9839253, `Tuolumne River` = -2.9839253, 
+                                          `San Joaquin River` = -2.9839253, months = 0.35)){
 
   month_since <- ifelse(month <= avg_ocean_transition_month, 0, max(1, month - avg_ocean_transition_month))
 
